ca9fe3796d14082f5a763729d79c752bdfb4326e
Cameron Scott <33464785+CScott96@users.noreply.github.com>
2025-02-12 16:50:52 +0000

    Add in get human choice
    Using some basic input checks to ensure user enters a number between
    1 and 3, loop will continue until the user enters a correct number

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,31 @@
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    return choice;
+    let compChoice = Math.floor(Math.random() * 3);
+    return compChoice;
 }
+function getHumanChoice() {
+    let humanChoice = parseInt(
+        prompt(
+            "Please enter your choice: 1 is Rock, 2 is Paper and 3 is Scissors.",
+            "1"
+        )
+    );
+    let notNumber = true;
+    while (notNumber) {
+        if (
+            Number.isInteger(humanChoice) &&
+            humanChoice <= 3 &&
+            humanChoice > 0
+        ) {
+            return humanChoice;
+            notNumber = false;
+        } else {
+            humanChoice = parseInt(
+                prompt(
+                    "Please re-enter your choice: 1 is Rock, 2 is Paper and 3 is Scissors.",
+                    "1"
+                ));
+        }
+        
+    }
+}
+
