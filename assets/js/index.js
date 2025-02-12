d261c25610cb30de88f44574844b821112d39568
Cameron Scott <33464785+CScott96@users.noreply.github.com>
2025-02-12 21:51:21 +0000

    Add test round function
    Using simple if statements to track the comparisons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,60 @@
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random() * 3);
+    switch (compChoice) {
+        case 1:
+            compChoice = "rock";
+            break;
+        case 2:
+            compChoice = "paper";
+            break;
+        case 3:
+            compChoice = "scissors";
+            break;
+    }
     return compChoice;
 }
 function getHumanChoice() {
-    let humanChoice = prompt(
+    let playerChoice = prompt(
         "Please enter your choice: Rock, Paper or Scissors",
         "Rock"
     ).toLowerCase();
     let validation = true;
     while (validation) {
-        switch (humanChoice) {
-            case 'rock':
-            case 'paper':
-            case 'scissors':
+        switch (playerChoice) {
+            case "rock":
+            case "paper":
+            case "scissors":
                 validation = false;
                 break;
             default:
-                humanChoice = prompt('Please re-enter your choice: Rock, Paper or Scissors','Rock').toLowerCase();
+                playerChoice = prompt(
+                    "Please re-enter your choice: Rock, Paper or Scissors",
+                    "Rock"
+                ).toLowerCase();
         }
     }
-    return humanChoice;
+    return playerChoice;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+    console.log('Human chose: ' + humanChoice + ' Computer chose: ' + computerChoice);
+    if (humanChoice === computerChoice) {
+        console.log("draw, play again!");
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log('Human wins! Rock beats Scissors');
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log('Human wins! Paper beats Rock');
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        console.log('Human wins! Scissors beats Paper');
+    } else {
+        console.log('Computer wins! ' + computerChoice + ' beats ' + humanChoice + '. Try again.')
+    }
+}
+
+playRound(humanSelection, computerSelection)
